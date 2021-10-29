cd4c36086119d9bce53b3d0ebeac5f50a218ff1a
ss-x69 <67320809+ss-x69@users.noreply.github.com>
2021-10-29 19:50:13 +0400

    Add files via upload

---
+++ notepad/notepad/NoteViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NoteViewController.swift
+//  notepad
+//
+//  Created by Subhodh  on 24/10/2021.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var notelabel: UITextView!
+    
+    public var noteTitle: String = ""
+    public var note: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
